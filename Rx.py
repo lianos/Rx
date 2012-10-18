7bb3069e17656fd5b645e9d3eb3ce8342c059202
Steve Lianoglou <slianoglou@gmail.com>
2012-10-18 16:33:01 -0400

    tidying code

--- Rx.py
+++ Rx.py
@@ -6,24 +6,9 @@ import subprocess
 import string
 import re
 
-## This file started its life as Rtools.py
-## https://github.com/karthikram/Rtools/blob/master/Rtools.py
-
-# rx_settings = sublime.load_settings('Rx.sublime-settings')
-# r_scope_regex = rx_settings.get('r_scope_regex')
-# r_scope_regex = re.compile(r_scope_regex)
-# rapp = rx_settings.get('Rapp')
-
-rx_settings = sublime.load_settings('Rx.sublime-settings')
-
-cmdr = None
-try:
-    cmdr = Rcommander()
-except (UnsupportedPlatformError) as (e):
-    sublime.error_message(__name__ + ": " + str(e))
-    print str(e)
-
-cmdr = Rcommander()
+## =============================================================================
+## Global variables and initialization are at the bottom of the file
+## =============================================================================
 
 class UnsupportedPlatformError(Exception):
     pass
@@ -36,25 +21,29 @@ class SendToRsessionCommand(sublime_plugin.TextCommand):
         return str
 
     def is_r_scope(self, region, regex=None):
-        # Check if the cursor / selection is in an R source scope
-        # Using region.begin() instead of region.b for the case when
-        # you are in a highlighted region/block. The curor can be at
-        # the 0 column of a new line which you don't want to execute
-        # but this will be out of R scope, eg. imagine the block is
-        # highlighted and the cursor is at ^, this might be out of
-        # source.r scope::
-        #
-        #    ```{r}
-        #    a <- 1:10
-        #    b <- 1:10
-        #    plot(a,b)
-        #    ^```
+        """Checks region to see if it is scoped as R code
+
+        Using region.begin() instead of region.b for the case when you are in a
+        highlighted region/block. The curor can be at the 0 column of a new line
+        which you don't want to execute but this will be out of R scope, eg.
+        imagine the block is highlighted and the cursor is at ^, this might be
+        out of source.r scope::
+
+            ```{r}
+            a <- 1:10
+            b <- 1:10
+            plot(a,b)
+            ^```
+
+        """
         if regex is None:
             regex = re.compile(rx_settings.get('r_scope_regex'))
         scope = self.view.scope_name(region.begin())
         return regex.search(scope) is not None
 
     def run(self, edit):
+        # The nuts and bolts of this method were taken from Rtools:
+        # https://github.com/karthikram/Rtools/blob/master/Rtools.py
         global rx_settings
         global cmdr
         if cmdr is None:
@@ -121,25 +110,26 @@ class SendToRsessionCommand(sublime_plugin.TextCommand):
 class Rcommander(object):
 
     def __init__(self):
-        platform = sys.platform
         self.app = rx_settings.get("Rapp")
-        if platform == "darwin":
-            self.send_code = self.send_code_darwin
-        elif platform == "nt":
-            self.send_code = self.send_code_win
-        elif platform == "linux2":
+        platform = sublime.platform()
+        if "osx" in platform:
+            self.send_code = self.send_code_osx
+        elif "linux" in platform:
             self.send_code = self.send_code_linux
+        elif "windows" in platform:
+            self.send_code = self.send_code_win
         else:
-            raise UnsupportedPlatformError("Unknown platform: %s" % platform)
+            msg = "Unknown platform/OS: (%s/%s)" % (sys.platform, os.name)
+            raise UnsupportedPlatformError(msg)
 
-    def send_code_darwin(self, selection):
+    def send_code_osx(self, selection):
         app = self.app
         args = ['osascript']
         for part in selection:
             args.extend(['-e', 'tell app "%s" to cmd "' % app + part + '"\n'])
         subprocess.Popen(args)
 
-    def send_code_nt(self, selection):
+    def send_code_win(self, selection):
         raise UnsupportedPlatformError("Windows platform not yet supported")
 
     def send_code_linux(self, selection):
@@ -151,26 +141,34 @@ class JumpToRsessionCommand(sublime_plugin.TextCommand):
         args = ['osascript', '-e', 'tell app "R64" to activate\n']
         subprocess.Popen(args)
 
-class RoxyDocsCommand(sublime_plugin.TextCommand):
+class RoxygenDocsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        # ========= From Rtools ===
+        # sel = self.view.sel()[0]
+        # params_reg = self.view.find('(?<=\().*(?=\))', sel.begin())
+        # params_txt = self.view.substr(params_reg)
+        # params = params_txt.split(',')
+        # snippet = "#'<brief desc>\n#'\n#'<full description>\n"
+        # for p in params:
+        #     snippet += "#' @param %s <what param does>\n" % p
+        # snippet += "#' @export\n#' @keywords\n#' @seealso\n#' @return\n#' @alias\n#' @examples \dontrun{\n#'\n#'}\n"
+        # self.view.insert(edit, sel.begin(), snippet)
+        # ==========================
         pass
 
-# class RDocsCommand(sublime_plugin.TextCommand):
-#     def run(self, edit):
-#         sel = self.view.sel()[0]
-#
-#         params_reg = self.view.find('(?<=\().*(?=\))', sel.begin())
-#         params_txt = self.view.substr(params_reg)
-#         params = params_txt.split(',')
-#
-#         snippet = "#'<brief desc>\n#'\n#'<full description>\n"
-#
-#         for p in params:
-#             snippet += "#' @param %s <what param does>\n" % p
-#
-#         snippet += "#' @export\n#' @keywords\n#' @seealso\n#' @return\n#' @alias\n#' @examples \dontrun{\n#'\n#'}\n"
-#
-#         self.view.insert(edit, sel.begin(), snippet)
+## =============================================================================
+##                    Global variables and initialization
+## =============================================================================
 
+rx_settings = sublime.load_settings('Rx.sublime-settings')
+cmdr = None
+
+try:
+    cmdr = Rcommander()
+except (UnsupportedPlatformError) as (e):
+    sublime.error_message(__name__ + ": " + str(e))
+    print str(e)
+
+cmdr = Rcommander()
 
 
